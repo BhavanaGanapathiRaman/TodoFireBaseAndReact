972accb635e79ec10e85d6d2672c62432b3dd288
BhavanaGanapathiRaman <bhavanabhavs698@gmail.com>
2020-10-23 19:29:48 -0500

    Modified UI phase 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
+import { Button,FormControl,Input,InputLabel } from "@material-ui/core";
+
 
 function App() {
-    return <div className = "App" > < /div>;
+  const [todos, setTodos] = useState(["DO THIS REACT APP", "DO DISHES"]);
+  const [input, setInput] = useState("");
+  console.log("input:" + input);
+  const addTodo = (event) => {
+    //fire off when we click the button
+
+    setTodos([...todos, input]);
+    event.preventDefault();
+    setInput("");
+  };
+  return (
+    <div className="App">
+     
+      <form>
+        {/* <input
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        /> */}
+        <FormControl>
+          <InputLabel htmlFor="my-input">Enter a todo</InputLabel>
+          <Input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+        </FormControl>
+        <Button
+          type="submit"
+          disabled={!input}
+          onClick={addTodo}
+          variant="contained"
+          color="primary"
+        >
+          Add TODO item
+        </Button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
